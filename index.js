53bda1bbb4b99ec57d76f5a3c437cb7f15960ba4
draganov-v <draganovvladimir90@gmail.com>
2022-11-06 20:05:50 +0000

    Use types & home jobs

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-alert(505)
-alert(234)
+console.log(505)
+console.log(2.34)
+console.log("qwerty")
+console.log(false)
+console.log(null)
+console.log(undefined)
+
+//console.log(userFirstName)
+
+let userFirstName;
+console.log(userFirstName)
+uesrFirstName = 'Bred';
+userFirstName = "O'Donel";
+userFirstName = 'O\'Donel';
+
+let isSunInSkyNow = false
+
+let peopleAge = '23';
+console.log(peopleAge)
+
+let isAdulperson = 'true';
+
+let programName;
+console.log(programName)
+programNamwe = 'C++';
+
+let isPersonHomeAnimal = 'no';
